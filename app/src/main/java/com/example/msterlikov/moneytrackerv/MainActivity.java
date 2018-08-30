c64d5d839383b9f801531f7e7954a8cd2afd0306
m.sterlikov <Mihei-75@yandex.ru>
2018-08-30 17:01:50 +0300

    init commit1

--- app/src/main/java/com/example/msterlikov/moneytrackerv/MainActivity.java
+++ app/src/main/java/com/example/msterlikov/moneytrackerv/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.msterlikov.moneytrackerv;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i( "MainActivity", "onResume: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i( "MainActivity", "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i( "MainActivity", "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i( "MainActivity", "onDestroy: ");
     }
 }
